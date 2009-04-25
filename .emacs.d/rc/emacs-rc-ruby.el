2d66892cc41b6c39f5f8f3fdd600fadfe767c781
Aleksei Gusev <aleksei.gusev@gmail.com>
2009-04-25 12:27:20 +0300

    removed ruby-mode from the configs.

--- .emacs.d/rc/emacs-rc-ruby.el
+++ .emacs.d/rc/emacs-rc-ruby.el
@@ -28,16 +28,6 @@
 
 ;;; ruby-mode site-lisp configuration
 
-(add-to-list 'load-path "~/.emacs.d/packages/ruby-mode")
-
-(autoload 'ruby-mode "ruby-mode" "Major mode to edit ruby files." t)
-
-(add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
-(add-to-list 'interpreter-mode-alist  '("ruby" . ruby-mode))
-
-(autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process" t)
-(autoload 'inf-ruby-keys "inf-ruby" "Set local key defs for inf-ruby in ruby-mode")
-
 (add-hook 'ruby-mode-hook 'inf-ruby-keys)
 
 (add-hook 'ruby-mode-hook (lambda ()
---
+++ Makefile
@@ -0,0 +1,4 @@
+install_configs:
+	for each in $$( ls -a1 | egrep -v '(Makefile|\.git|^\.\.?$$|\~$$)'); do \
+	  ln -s `pwd`/$$each $$HOME/$$each; \
+	done;
