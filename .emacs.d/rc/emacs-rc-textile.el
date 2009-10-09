395910dcf0493e7545ce4986d1ed4d98ec3153a7
Aleksei Gusev <aleksei.gusev@gmail.com>
2009-10-09 15:54:38 +0300

    removed auto-fill-mode from textile-mode, added visual-line-mode.

--- .emacs.d/rc/emacs-rc-textile.el
+++ .emacs.d/rc/emacs-rc-textile.el
@@ -1,3 +1,8 @@
 (require 'textile-mode)
 
 (add-to-list 'auto-mode-alist '("\\.textile\\'" . textile-mode))
+
+(add-hook 'textile-mode-hook
+					'(lambda ()
+						 (auto-fill-mode 0)
+						 (visual-line-mode 1)))
