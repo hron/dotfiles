ce1ec84f40d91c8083e22c0534ad88df621083ee
Aleksei Gusev <aleksei.gusev@gmail.com>
2009-10-13 07:16:34 +0300

    added auto detection of redmine wiki editing pages (textile-mode).

--- .emacs.d/rc/emacs-rc-textile.el
+++ .emacs.d/rc/emacs-rc-textile.el
@@ -2,6 +2,8 @@
 
 (add-to-list 'auto-mode-alist '("\\.textile\\'" . textile-mode))
 
+(add-to-list 'auto-mode-alist '("/tmp/.*redmine.*wiki" . textile-mode))
+
 (add-hook 'textile-mode-hook
 					'(lambda ()
 						 (auto-fill-mode 0)
