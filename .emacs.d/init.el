d1ce5b25f60df6f3330b6f6b8be526a173dceacd
Aleksei Gusev <aleksei.gusev@gmail.com>
2009-10-20 00:14:54 +0300

    moved sessions, added desktop-save-mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -77,6 +77,44 @@
 (load "~/.emacs.d/rc/emacs-rc-smtpmail")
 (load "~/.emacs.d/rc/emacs-rc-ediff")
 
+;;----------------------------------------------------------------------------
+;; Desktop saving
+;;----------------------------------------------------------------------------
+;; save a list of open files in ~/.emacs.d/.emacs.desktop
+;; save the desktop file automatically if it already exists
+(setq desktop-path '("~/.emacs.d"))
+(setq desktop-save 'if-exists)
+(desktop-save-mode 1)
+
+
+;;----------------------------------------------------------------------------
+;; Restore histories and registers after saving
+;;----------------------------------------------------------------------------
+(require 'session)
+(setq session-save-file (expand-file-name "~/.emacs.d/.session"))
+(add-hook 'after-init-hook 'session-initialize)
+
+;; save a bunch of variables to the desktop file
+;; for lists specify the len of the maximal saved data also
+(setq desktop-globals-to-save
+      (append '((extended-command-history . 30)
+                (file-name-history        . 100)
+                (ido-last-directory-list  . 100)
+                (ido-work-directory-list  . 100)
+                (ido-work-file-list       . 100)
+                (grep-history             . 30)
+                (compile-history          . 30)
+                (minibuffer-history       . 50)
+                (query-replace-history    . 60)
+                (read-expression-history  . 60)
+                (regexp-history           . 60)
+                (regexp-search-ring       . 20)
+                (search-ring              . 20)
+                (shell-command-history    . 50)
+                tags-file-name
+                register-alist)))
+
+
 ;; Control use of local variables in files you visit.
 ;; The value can be t, nil, :safe, :all, or something else.
 ;; 
