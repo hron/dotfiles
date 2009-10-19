4d5127f7fff84003c9783e71a16fdbe795210f70
Aleksei Gusev <aleksei.gusev@gmail.com>
2009-10-20 00:25:04 +0300

    moved custom related lisp code to separate file.

---
+++ .emacs.d/custom.el
@@ -0,0 +1,16 @@
+(put 'scroll-left 'disabled nil)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(canlock-password "e6a803bd4bbe7baa935108fb943f3df19651b148")
+ '(safe-local-variable-values (quote ((phpunit-run-directory . "/home/gusev/src/z/zanby/core") (js2-strict-missing-semi-warning) (js2-strict-missing-semi-warning . 100) (c-hanging-comment-ender-p) (folded-file . t) (folding-internal-margins) (sgml-omittag . t) (sgml-shorttag . t) (sgml-minimize-attributes) (sgml-always-quote-attributes . t) (sgml-indent-step . 2) (sgml-indent-data . t) (sgml-parent-document) (sgml-default-dtd-file) (sgml-exposed-tags) (sgml-local-catalogs) (sgml-local-ecat-files) (encoding . koi8-r)))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+
+(put 'set-goal-column 'disabled nil)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -140,23 +140,13 @@
 ;; and ignores this variable.
 (setq enable-local-variables :all)
 
+;;----------------------------------------------------------------------------
+;; Variables configured via the interactive 'customize' interface
+;;----------------------------------------------------------------------------
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 ;; Color theme loading, must be the last.
 (load-library "color-themes/color-theme-dark-hron")
 (color-theme-dark-hron)
 
-(put 'scroll-left 'disabled nil)
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(canlock-password "e6a803bd4bbe7baa935108fb943f3df19651b148")
- '(safe-local-variable-values (quote ((phpunit-run-directory . "/home/gusev/src/z/zanby/core") (js2-strict-missing-semi-warning) (js2-strict-missing-semi-warning . 100) (c-hanging-comment-ender-p) (folded-file . t) (folding-internal-margins) (sgml-omittag . t) (sgml-shorttag . t) (sgml-minimize-attributes) (sgml-always-quote-attributes . t) (sgml-indent-step . 2) (sgml-indent-data . t) (sgml-parent-document) (sgml-default-dtd-file) (sgml-exposed-tags) (sgml-local-catalogs) (sgml-local-ecat-files) (encoding . koi8-r)))))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
-
-(put 'set-goal-column 'disabled nil)
