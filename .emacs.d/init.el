84095c5809137ff0c3ae18eaf16d1040c4a90e6c
Aleksei Gusev <aleksei.gusev@gmail.com>
2009-10-22 00:57:19 +0300

    added elpa.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -116,3 +116,14 @@
 (require 'emacs-rc-misc-things)
 
 
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+		(load
+		 (expand-file-name "~/.emacs.d/elpa/package.el"))
+	(package-initialize))
