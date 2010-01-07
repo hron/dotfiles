bcb20229ef3ec7bd8e2f48cd0a7bbdd03a029077
Aleksei Gusev <aleksei.gusev@gmail.com>
2010-01-07 19:54:28 +0200

    relative stable configuration with almost all needed features.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,30 +1,136 @@
 import XMonad
+import XMonad.ManageHook
+import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.RestoreMinimized
+import XMonad.Hooks.ManageHelpers
+import XMonad.Actions.CycleWS
+import XMonad.Actions.GridSelect
+import XMonad.Actions.WindowMenu
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig
+import XMonad.Util.XSelection
+import XMonad.Util.NamedScratchpad
+import XMonad.Prompt
+import XMonad.Prompt.Window
+import XMonad.Prompt.Workspace
+import XMonad.Prompt.Ssh
+import XMonad.Prompt.Shell
+import XMonad.Prompt.XMonad
+import XMonad.Layout.Maximize
+import XMonad.Layout.Minimize
+import XMonad.Layout.CenteredMaster
+import XMonad.Layout.AutoMaster
+import XMonad.Layout.Grid
+import qualified XMonad.Layout.Magnifier as Mag
+    
+import qualified XMonad.StackSet as W
+
 import System.IO
-
-myManageHook = composeAll
-    [ className =? "Gimp"      --> doFloat
-    , className =? "Vncviewer" --> doFloat
-    ]
-
+import Data.Monoid
+    
 main = do
-    xmonad $ defaultConfig
+  xmonad $ withUrgencyHookC dzenUrgencyHook urgencyConfig { suppressWhen = Focused }
+         $ gnomeConfig
        { manageHook = manageDocks <+>
                       myManageHook <+>
-                      manageHook defaultConfig
-       , layoutHook = avoidStruts  $  layoutHook defaultConfig
-       , modMask = mod4Mask -- Rebind Mod to the Windows key
-       } `additionalKeys`
-       [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-       , ((mod4Mask, xK_t), spawn "uxterm -e 'byobu -R'")
-       , ((mod4Mask, xK_x), spawn "uxterm -e 'ncmpcpp'")
-       , ((mod4Mask .|. shiftMask, xK_t), spawn "uxterm")
-       , ((mod4Mask, xK_b), spawn "/home/gusev/bin/conkeror")
-       , ((mod4Mask, xK_e), spawn "emacsclient -nc")
+                      manageHook gnomeConfig
+       , layoutHook = myLayoutHook
+       -- Rebind Mod to the Windows key
+       , modMask = mod4Mask
+       , handleEventHook = mappend myHandleEventHook (handleEventHook gnomeConfig)
+       }
+       `removeKeysP`
+       [
+         ("M-S-<Return>")
+       , ("M-p")
+       , ("M-S-p")
+       -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
+       -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
+       , ("M-w")
+       , ("M-e")
+       , ("M-r")
+       , ("M-S-w")
+       , ("M-S-e")
+       , ("M-S-r")
        ]
+       `additionalKeysP`
+       [
+       -- DE actions
+         ("M-S-l", spawn "gnome-screensaver-command -l")
+       , ("M-S-q", spawn "gnome-session-save --gui --logout-dialog")
+       -- External applications
+       , ("M-t",   spawn "uxterm -e 'byobu -R'")
+       , ("M-x",   spawn "uxterm -e 'ncmpcpp'")
+       , ("M-S-t", spawn "uxterm")
+       , ("M-b",   spawn "/home/gusev/bin/conkeror")
+       , ("M-e",   spawn "emacsclient -nc")
+       -- , ("M-q",   spawn "stardict ")
+       , ("M-q",   namedScratchpadAction scratchpads "stardict")
+       -- WM actions
+       , ("M-<Escape>", toggleWS)
+       , ("M-S-g", goToSelected defaultGSConfig)
+       , ("M-g", windowPromptGoto  defaultXPConfig)
+       , ("M-s", sshPrompt defaultXPConfig)
+       , ("M-r", shellPrompt amberXPConfig)
+       , ("M-u", focusUrgent)   -- FIXME: do not work
+       , ("M-o", windowMenu)
+       , ("M-S-r", xmonadPrompt amberXPConfig)
+       , ("M-f", fullFloatFocused)
+       , ("M-v", withFocused $ windows . W.sink)
+       , ("M-m", sendMessage Mag.Toggle)
+       ]
+       `additionalKeys`
+       -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
+       -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
+       [((m .|. mod4Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+            | (key, sc) <- zip [xK_F1, xK_F2, xK_F3] [0..]
+       , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+myLayoutHook = minimize $ maximize $ avoidStruts $ Mag.magnifierOff
+               (tiled ||| (Mirror tiled) ||| Full ||| layoutHook gnomeConfig)
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
+
+
+myManageHook = composeAll
+    [ className =? "Gimp"                 --> doFloat
+    , className =? "Vncviewer"            --> doFloat
+    , className =? "stardict"             --> doFloat
+    , className =? "gcolor2"              --> doFloat
+    , className =? "totem"                --> doFloat
+    , className =? "Firefox Preferences"  --> doFloat
+    , className =? "*VLC"                 --> doFloat
+    , className =? "Pidgin"               --> doFloat
+    , className =? "Skype"                --> doFloat
+    , isDialog                            --> doCenterFloat
+    , isFullscreen                        --> doFullFloat
+    ]
+               
+myHandleEventHook = restoreMinimizedEventHook
+
+-- http://www.haskell.org/pipermail/xmonad/2009-August/008365.html
+fullFloatFocused = withFocused $ \f -> windows =<< appEndo `fmap` runQuery doFullFloat f
+
+scratchpads = [
+ -- run stardict, find it by class name, place it in the floating window
+ -- 1/6 of screen width from the left, 1/6 of screen height
+ -- from the top, 2/3 of screen width by 2/3 of screen height
+     NS "stardict" "stardict" (className =? "Stardict")
+         (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
+ ]
 
 -- Local variables:
 -- compile-command: "xmonad --recompile && xmonad --restart"
