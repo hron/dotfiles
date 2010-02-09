9fe6532e635760ceff0dd8d88aecd636f0802cf4
Aleksei Gusev <aleksei.gusev@gmail.com>
2010-02-09 09:13:47 +0200

    added org-mode customization.

--- .emacs.d/rc/emacs-rc-org.el
+++ .emacs.d/rc/emacs-rc-org.el
@@ -30,10 +30,29 @@
 (setq org-hide-leading-stars t)
 
 (add-hook 'message-mode-hook 'turn-on-orgstruct)
+(add-hook 'message-mode-hook 'turn-on-orgtbl)
 
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+;; You can insert and follow links that have Org syntax not only in Org, but in
+;; any Emacs buffer. For this, you should create two global commands, like this
+;; (please select suitable global keys yourself):
+(global-set-key "\C-cL" 'org-insert-link-global)
+(global-set-key "\C-cO" 'org-open-at-point-global)
+
+;; The most basic logging is to keep track of _when_ a certain TODO item
+;; was finished.  This is achieved with(1).
+;; 
+;; [[info:org:Closing%20items][info:org:Closing items]]
+(setq org-log-done 'time)
+
+;; [[info:org:Setting%20up%20Remember][info:org:Setting up Remember]]
+(org-remember-insinuate)
+(setq org-directory "~/org/")
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+(define-key global-map "\C-cR" 'org-remember)
 
 (provide 'emacs-rc-org)
 ;;; emacs-rc-org.el ends here
