7c3ee8fbcfdffeaf75974ae9deb0ce75cdc505c9
Aleksei Gusev <aleksei.gusev@gmail.com>
2010-04-02 09:13:41 +0300

    added eldoc-mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -65,6 +65,11 @@
 				   ;; (turn-on-orgtbl)
 				   (highlight-parentheses-mode 1)))
 
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+
+
 ;; Text
 (add-hook 'text-mode-hook '(lambda ()
 			     (turn-on-auto-fill)
