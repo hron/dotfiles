2a176a4a79a13571028877566a443a26104bdc62
Aleksei Gusev <aleksei.gusev@gmail.com>
2010-04-12 22:15:39 +0300

    customize.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(canlock-password "e6a803bd4bbe7baa935108fb943f3df19651b148")
- '(safe-local-variable-values (quote ((phpunit-run-directory . "/home/gusev/src/z/zanby/core") (js2-strict-missing-semi-warning) (js2-strict-missing-semi-warning . 100) (c-hanging-comment-ender-p) (folded-file . t) (folding-internal-margins) (sgml-omittag . t) (sgml-shorttag . t) (sgml-minimize-attributes) (sgml-always-quote-attributes . t) (sgml-indent-step . 2) (sgml-indent-data . t) (sgml-parent-document) (sgml-default-dtd-file) (sgml-exposed-tags) (sgml-local-catalogs) (sgml-local-ecat-files) (encoding . koi8-r)))))
+ '(safe-local-variable-values (quote ((eeb-defaults eeel4r ee-delimiter-hash nil t t) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby") (test-script . "../testing/test-el4r.rb") (modes ruby-mode emacs-lisp-mode) (phpunit-run-directory . "/home/gusev/src/z/zanby/core") (js2-strict-missing-semi-warning) (js2-strict-missing-semi-warning . 100) (c-hanging-comment-ender-p) (folded-file . t) (folding-internal-margins) (sgml-omittag . t) (sgml-shorttag . t) (sgml-minimize-attributes) (sgml-always-quote-attributes . t) (sgml-indent-step . 2) (sgml-indent-data . t) (sgml-parent-document) (sgml-default-dtd-file) (sgml-exposed-tags) (sgml-local-catalogs) (sgml-local-ecat-files) (encoding . koi8-r)))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
