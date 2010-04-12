59b10c9228adfd80dc0a0680a24f484dc8956812
Aleksei Gusev <aleksei.gusev@gmail.com>
2010-04-12 23:22:20 +0300

    added el4r customization.

---
+++ .el4r/init.rb
@@ -0,0 +1,5 @@
+# Beginning of the el4r block:
+# RCtool generated this block automatically. DO NOT MODIFY this block!
+# This is the el4r initialization file.
+# End of the el4r block.
+# User-setting area is below this line.
