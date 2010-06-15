282cebe355b146aae5f88e098d3087a099115f5a
Aleksei Gusev <aleksei.gusev@gmail.com>
2010-06-15 11:34:34 +0300

    emms.

--- .emacs.d/rc/emacs-rc-emms.el
+++ .emacs.d/rc/emacs-rc-emms.el
@@ -34,7 +34,7 @@
 (setq emms-player-mpd-server-name "localhost"
       emms-player-mpd-server-port "6600")
 
-(setq emms-player-mpd-music-directory "~/Музыка")
+(setq emms-player-mpd-music-directory "~/Music")
 
 (add-to-list 'emms-info-functions 'emms-info-mpd)
 (add-to-list 'emms-player-list 'emms-player-mpd)
