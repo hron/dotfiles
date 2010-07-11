988de121d7abbbb6f43e18c9088537bc3eca4a9e
Aleksei Gusev <aleksei.gusev@gmail.com>
2010-07-11 12:18:27 +0300

    stuff.

--- .emacs.d/rc/emacs-rc-emms.el
+++ .emacs.d/rc/emacs-rc-emms.el
@@ -56,18 +56,20 @@
 ;;     ))
 
 (add-hook 'emms-browser-show-display-hook
-	  '(lambda ()
-	     (local-set-key (kbd "n") 'next-line)
-	     (local-set-key (kbd "j") 'next-line)
-	     (local-set-key (kbd "p") 'previous-line)
-	     (local-set-key (kbd "k") 'previous-line)))
+          '(lambda ()
+             (local-set-key (kbd "n") 'next-line)
+             (local-set-key (kbd "j") 'next-line)
+             (local-set-key (kbd "p") 'previous-line)
+             (local-set-key (kbd "k") 'previous-line)))
 
 
 ;; (if (eq window-system 'x)
+
+
 ;;     (add-hook 'emms-player-started-hook
 ;;        '(lambda ()
-;; 	  (shell-command (concat "notify-send -t 3000 "
-;; 				 "\"" (emms-show) "\"")))))
+;;        (shell-command (concat "notify-send -t 3000 "
+;;                               "\"" (emms-show) "\"")))))
 
 (provide 'emacs-rc-emms)
 ;;; emacs-rc-emms.el ends here
