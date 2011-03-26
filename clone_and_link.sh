9991391103a7ad3a7bc753b96b975422b2f26662
Aleksei Gusev <aleksei.gusev@gmail.com>
2011-03-26 20:00:53 +0200

    changed clone_and_link.sh to have ~/.dot-files to meet requirements of babushka dep.

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
-[[ -d $HOME/src ]] || mkdir $HOME/src && cd $HOME/src &&
-git clone git://github.com/hron/dotfiles &&
-cd $HOME/src/dotfiles && make
+# [[ -d $HOME/src ]] || mkdir $HOME/src && cd $HOME/src &&
+# git clone git://github.com/hron/dotfiles &&
+# cd $HOME/src/dotfiles && make
+
+cd &&
+ [ -d '.dot-files' ] || git clone git://github.com/hron/dotfiles .dot-files &&
+ cd .dot-files && make
