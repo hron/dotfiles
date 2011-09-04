b367f01606bde9aa3c1a58955e8fbd7ca5ddd33c
Aleksei Gusev <aleksei.gusev@gmail.com>
2011-09-04 20:40:13 +0300

    removed espresso-mode.

--- .emacs.d/rc/emacs-rc-javascript.el
+++ .emacs.d/rc/emacs-rc-javascript.el
@@ -3,18 +3,7 @@
 
 (setq js-indent-level 2)
 (add-to-list 'auto-mode-alist '("\\.js" . espresso-mode))
-(autoload 'espresso-mode "espresso" nil t)
 
 (autoload 'inferior-moz-mode "chrome_content_moz" "MozRepl Inferior Mode" t)
 (autoload 'moz-minor-mode "chrome_content_moz" "Mozilla Minor and Inferior Mozilla Modes" t)
-
-(add-hook 'espresso-mode-hook '(lambda ()
-                                 (flyspell-prog-mode)
-                                 (moz-minor-mode 1)
-                                 ;; (turn-on-orgstruct)
-                                 ;; (turn-on-orgtbl)
-                                 (highlight-parentheses-mode 1)))
-
-(setq espresso-indent-level 2)
-
 (provide 'emacs-rc-javascript)
