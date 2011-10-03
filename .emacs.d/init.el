4dae0f5ad64390f22204e3da07d5b65a4bdd9a55
Aleksei Gusev <aleksei.gusev@gmail.com>
2011-10-03 20:36:24 +0300

    installed gist-mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -232,9 +232,9 @@ argument allows editing of the server command arguments."
 ;; This prevents activating nxhtml-mode for .html.haml files.
 (add-to-list 'auto-mode-alist '("\\.html\\.haml$" . haml-mode))
 
-;; autopair.el
-(require 'autopair)
-(autopair-global-mode) ;; to enable in all buffers
+
+(setq aug-secret-file "~/.emacs.d/.secret-file")
+(load aug-secret-file t)
 
 ;;
 ;; Emacs core customization
