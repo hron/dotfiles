5341e3e74d139993029d59bc267385bdc0f92fe3
Aleksei Gusev <aleksei.gusev@gmail.com>
2011-11-28 20:45:58 +0300

    updated apache-mode from ELPA.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -79,8 +79,12 @@
 (setq js-indent-level 2)
 
 ;; Apache
-(require 'apache-mode)
-(add-to-list 'auto-mode-alist '(".*/etc/apache.*" . apache-mode))
+(autoload 'apache-mode "apache-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.htaccess\\'"   . apache-mode))
+(add-to-list 'auto-mode-alist '("httpd\\.conf\\'"  . apache-mode))
+(add-to-list 'auto-mode-alist '("srm\\.conf\\'"    . apache-mode))
+(add-to-list 'auto-mode-alist '("access\\.conf\\'" . apache-mode))
+(add-to-list 'auto-mode-alist '("sites-\\(available\\|enabled\\)/" . apache-mode))
 
 ;; Textile
 (require 'emacs-rc-textile)
