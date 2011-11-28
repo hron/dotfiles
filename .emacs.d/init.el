13a51c51cccc1d913c43a3bb4263f2fcfb05aad3
Aleksei Gusev <aleksei.gusev@gmail.com>
2011-11-28 20:09:32 +0300

    removed edit server.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -44,9 +44,6 @@
 
 (require 'server)
 
-;; Edit with Emacs extension (https://github.com/stsquad/emacs_chrome)
-(require 'edit-server)
-
 ;;----------------------------------------------------------------------------
 ;; Handier way to add modes to auto-mode-alist
 ;;----------------------------------------------------------------------------
