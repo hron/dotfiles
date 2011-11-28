c513d6f3eeac8c7865a5eccb1cb8a78902ddb82e
Aleksei Gusev <aleksei.gusev@gmail.com>
2011-11-28 20:39:49 +0300

    deleted gentoo-syntax mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -75,9 +75,6 @@
                              ;; (turn-on-orgtbl)
                              (highlight-parentheses-mode 1)))
 
-;; Gentoo Syntax (ebuild, init.d-scripts and etc)
-(require 'gentoo-syntax)
-
 (add-to-list 'auto-mode-alist
              '("/etc/conf.d/" . sh-mode))
 
