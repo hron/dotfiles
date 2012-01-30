01020f09a51c77303f92b2c775aa77267ad7fc4d
Aleksei Gusev <aleksei.gusev@gmail.com>
2012-01-30 15:38:33 +0300

    Install ibus support.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -173,6 +173,12 @@
                                (make-variable-buffer-local 'electric-indent-chars)
                                (setq electric-indent-chars '())))
 
+(require 'ibus)
+;; (add-hook 'after-init-hook 'ibus-mode-on)
+(ibus-mode-on)
+(global-set-key (kbd "S-<delete>") 'ibus-toggle)
+(global-set-key (kbd "S-<return>") 'ibus-toggle)
+
 ;;
 ;; Emacs core customization
 ;;
