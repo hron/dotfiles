bb2fa0782c7a9981092e6d106009571976d6009b
Aleksei Gusev <aleksei.gusev@gmail.com>
2012-02-27 21:52:25 +0300

    Fixed showing scheduled for today at specific time todos in org-mode.

--- .emacs.d/rc/emacs-rc-org.el
+++ .emacs.d/rc/emacs-rc-org.el
@@ -140,7 +140,7 @@ in current buffer."
 (setq org-return-follows-link t)
 
 (setq
- org-agenda-scheduled-later-expr "-SCHEDULED>\"<today>\"-DEADLINE>\"<today>\"-someday/"
+ org-agenda-scheduled-later-expr "-SCHEDULED>=\"<tomorrow>\"-DEADLINE>=\"<tomorrow>\"-someday/"
  org-agenda-na-expr (concat org-agenda-scheduled-later-expr "TODO")
  org-agenda-active-expr (concat org-agenda-scheduled-later-expr "-DONE"))
 
