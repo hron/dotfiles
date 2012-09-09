90d5daf660381de9a5430776fce12cc5ad5b5d64
Aleksei Gusev <aleksei.gusev@gmail.com>
2012-09-09 11:27:29 +0300

    Add mapping for 2combinators' servers.

--- .emacs.d/rc/emacs-rc-tramp.el
+++ .emacs.d/rc/emacs-rc-tramp.el
@@ -47,10 +47,13 @@
                  '(nil "root" "/ssh:%h:"))
 
     (add-to-list 'tramp-default-proxies-alist
-                 '("wyvoxapp.com" "deployer" "/ssh:aleksei@%h:"))
+                 '("\\(wyvoxapp.com\\|spinstudioapp.com\\)" "deployer" "/ssh:aleksei@%h:"))
 
     (add-to-list 'tramp-default-proxies-alist
                  '("sfa\\(25\\|36\\|37\\|39\\)" "production" "/ssh:aleksei@%h:"))
+
+    (add-to-list 'tramp-default-proxies-alist
+                 '("sfa15" "root" "/ssh:aleksei@fire.boutiqueair.com:"))
 
     (add-to-list 'tramp-default-proxies-alist
                  '("sfa35" "\\(cruise\\|staging\\|production\\|proto\\|jenkins\\)" "/ssh:aleksei@%h:"))
