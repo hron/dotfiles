9e66b7a1b62b75795f95cbcf01e2c518eab8c1a5
Aleksei Gusev <aleksei.gusev@gmail.com>
2013-07-27 12:11:23 +0300

    Upgrade projectile.

--- .emacs.d/rc/emacs-rc-projectile.el
+++ .emacs.d/rc/emacs-rc-projectile.el
@@ -26,35 +26,5 @@
 
 (require 'projectile)
 
-(defun projectile-project-files (directory)
-  "List the files in DIRECTORY and in its sub-directories.
-Files are returned as relative paths to the project root."
-  ;; check for a cache hit first if caching is enabled
-  (let ((files-list (and projectile-enable-caching
-			 (gethash directory projectile-projects-cache)))
-	(root (projectile-project-root)))
-    ;; cache disabled or cache miss
-    (unless files-list
-      (if projectile-use-native-indexing
-	  (progn
-	    (message "Projectile is indexing %s. This may take a while."
-		     (propertize directory 'face 'font-lock-keyword-face))
-	    (setq files-list
-		  ;; we need the files with paths relative to the project root
-		  (-map (lambda (file) (s-chop-prefix root file))
-			(projectile-index-directory directory (projectile-patterns-to-ignore)))))
-	;; use external tools to get the project files
-	(let ((current-dir (if (buffer-file-name)
-			       (file-name-directory (buffer-file-name))
-			     default-directory)))
-	  (cd root)
-	  (setq files-list (-map (lambda (f)
-				   (s-chop-prefix root f))
-				 (projectile-get-repo-files)))
-	  ;; restore the original current directory
-	  (message current-dir)
-	  (cd current-dir))))
-    files-list))
-
 (provide 'emacs-rc-projectile)
 ;;; emacs-rc-projectile.el ends here
