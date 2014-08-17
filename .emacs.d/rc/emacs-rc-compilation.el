c470041a618efacbf29517f857839c5269dbf89b
Aleksei Gusev <aleksei.gusev@gmail.com>
2014-08-17 17:26:08 +0300

    Additional tricks for compilation buffer

--- .emacs.d/rc/emacs-rc-compilation.el
+++ .emacs.d/rc/emacs-rc-compilation.el
@@ -53,12 +53,18 @@
 (setq compilation-error-regexp-alist
       '(
 	;; Forget all errors on each guard run.
-	("Running\\(?: all\\|:\\)?" nil nil nil nil nil
+	("^\\(?:[0-9][0-9]:[0-9][0-9]:[0-9][0-9] - INFO - \\)?Running" nil nil nil nil nil
          (0 (progn (compilation-forget-errors)
                    nil)))
+	;; Bottom messages of rspec run
 	("\\(?:^rspec\\(?: -p [^[:space:]]+\\)?\\|#\\)\\(?: \\)\\([^\(].*\\):\\([1-9][0-9]*\\)" 1 2)
-	cucumber
-	ruby-Test::Unit
+	;; Bottom messages of cucumber run
+        ("\\(?:^cucumber\\(?: -p [^[:space:]]+\\)?\\|#\\)\\(?:
+      \\)\\([^\(].*\\):\\([1-9][0-9]*\\)" 1 2)
+	;; Ruby
+	("^[\t ]*\\(?:from \\)?\\([^\(\n][^[:space:]\n]*\\):\\([1-9][0-9]*\\)\\(:in `.*'\\)?.*$" 1 2)
+	;; Ruby's Test::Unit
+	("[\t ]*\\[\\([^\(].*\\):\\([1-9][0-9]*\\)\\(\\]\\)?:" 1 2)
 	gnu
 	bash
 	gcc-include
