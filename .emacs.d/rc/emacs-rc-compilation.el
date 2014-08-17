2e2d5797480e790507942bfeb9b0f70d36149449
Aleksei Gusev <aleksei.gusev@gmail.com>
2014-08-17 14:40:06 +0300

    Fix jumping to wrong lines from guard buffer.

--- .emacs.d/rc/emacs-rc-compilation.el
+++ .emacs.d/rc/emacs-rc-compilation.el
@@ -32,82 +32,26 @@
 
 (let ((compilation-regexps
        '((ruby
-       "^[\t ]*\\(?:from \\)?\\([^\(\n][^[:space:]\n]*\\):\\([1-9][0-9]*\\)\\(:in `.*'\\)?.*$" 1 2)
-      (ruby-Test::Unit
-       "[\t ]*\\[\\([^\(].*\\):\\([1-9][0-9]*\\)\\(\\]\\)?:" 1 2)
-      (rspec
-       "\\(?:^rspec\\(?: -p [^[:space:]]+\\)?\\|#\\)\\(?: \\)\\([^\(].*\\):\\([1-9][0-9]*\\)" 1 2)
-      (cucumber
-       "\\(?:^cucumber\\(?: -p [^[:space:]]+\\)?\\|#\\)\\(?: \\)\\([^\(].*\\):\\([1-9][0-9]*\\)" 1 2))))
+	  "^[\t ]*\\(?:from \\)?\\([^\(\n][^[:space:]\n]*\\):\\([1-9][0-9]*\\)\\(:in `.*'\\)?.*$" 1 2)
+	 (ruby-Test::Unit
+	  "[\t ]*\\[\\([^\(].*\\):\\([1-9][0-9]*\\)\\(\\]\\)?:" 1 2)
+	 (rspec
+	  "\\(?:^rspec\\(?: -p [^[:space:]]+\\)?\\|#\\)\\(?: \\)\\([^\(].*\\):\\([1-9][0-9]*\\)" 1 2)
+	 (cucumber
+	  "\\(?:^cucumber\\(?: -p [^[:space:]]+\\)?\\|#\\)\\(?:
+       \\)\\([^\(].*\\):\\([1-9][0-9]*\\)" 1 2)
+	 (ruby-Guard
+	  "^Running" 0 nil nil nil nil
+	  (0 (progn (compilation-forget-errors)
+		    nil)))
+	 )
+       ))
   (dolist (regexp compilation-regexps)
     (add-to-list 'compilation-error-regexp-alist (cdr regexp) t)))
 
 (add-hook 'compilation-mode-hook
           '(lambda ()
              (local-set-key "\C-cg" 'rgrep)))
-
-;; This is a redefined version of `compilation-next-error-function' which
-;; doesn't use markers at all. In other words is a hack for guard buffers.
-(defun compilation-next-error-function (n &optional reset)
-  "Advance to the next error message and visit the file where the error was.
-This is the value of `next-error-function' in Compilation buffers."
-  (interactive "p")
-  (when reset
-    (setq compilation-current-error nil))
-  (let* ((screen-columns compilation-error-screen-columns)
-         (first-column compilation-first-column)
-         (last 1)
-         (msg (compilation-next-error (or n 1) nil
-                                      (or compilation-current-error
-                                          compilation-messages-start
-                                          (point-min))))
-         (loc (compilation--message->loc msg))
-         (end-loc (compilation--message->end-loc msg))
-         (marker (point-marker)))
-    (setq compilation-current-error (point-marker)
-          overlay-arrow-position
-            (if (bolp)
-             compilation-current-error
-              (copy-marker (line-beginning-position))))
-    (with-current-buffer
-     (compilation-find-file
-         marker
-         (caar (compilation--loc->file-struct loc))
-         (cadr (car (compilation--loc->file-struct loc))))
-      (let ((screen-columns
-             ;; Obey the compilation-error-screen-columns of the target
-             ;; buffer if its major mode set it buffer-locally.
-             (if (local-variable-p 'compilation-error-screen-columns)
-                 compilation-error-screen-columns screen-columns))
-            (compilation-first-column
-             (if (local-variable-p 'compilation-first-column)
-                 compilation-first-column first-column)))
-     (save-restriction
-          (widen)
-          (goto-char (point-min))
-          ;; Treat file's found lines in forward order, 1 by 1.
-          (dolist (line (reverse (cddr (compilation--loc->file-struct loc))))
-            (when (car line)		; else this is a filename w/o a line#
-              (beginning-of-line (- (car line) last -1))
-              (setq last (car line)))
-            ;; Treat line's found columns and store/update a marker for each.
-            (dolist (col (cdr line))
-              (if (compilation--loc->col col)
-                  (if (eq (compilation--loc->col col) -1)
-                      ;; Special case for range end.
-                      (end-of-line)
-                    (compilation-move-to-column (compilation--loc->col col)
-                                             screen-columns))
-             (beginning-of-line)
-             (skip-chars-forward " \t"))
-              (if (compilation--loc->marker col)
-                  (set-marker (compilation--loc->marker col) (point))
-             (setf (compilation--loc->marker col) (point-marker)))
-              ;; (setf (compilation--loc->timestamp col) timestamp)
-              )))))
-    (compilation-goto-locus marker (compilation--loc->marker loc)
-                            (compilation--loc->marker end-loc))
-    (setf (compilation--loc->visited loc) t)))
 
 (provide 'emacs-rc-compilation)
 ;;; emacs-rc-compilation.el ends here
