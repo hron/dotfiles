a78b087888a54388958ab8f3782c8ee94867d4ee
Aleksei Gusev <aleksei.gusev@gmail.com>
2019-03-23 20:02:24 +0200

    Add my private layer for Spacemacs

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -43,13 +43,14 @@ values."
      emacs-lisp
      git
      ;; markdown
-     ;; org
+     org
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
      ;; spell-checking
      ;; syntax-checking
      ;; version-control
+     gusev
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -301,6 +302,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (setq evil-toggle-key "C-S-M-z")
   )
 
 (defun dotspacemacs/user-config ()
@@ -330,3 +332,26 @@ you should place your code here."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(defun dotspacemacs/emacs-custom-settings ()
+  "Emacs custom settings.
+This is an auto-generated function, do not modify its content directly, use
+Emacs customize menu instead.
+This function is called at the very end of Spacemacs initialization."
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org smeargle magit-svn magit-gitflow magit-popup helm-gitignore helm-git-grep gitignore-templates gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link evil-magit magit transient git-commit with-editor rvm ruby-tools ruby-test-mode rubocop rspec-mode robe rbenv rake minitest chruby bundler inf-ruby ws-butler winum which-key wgrep volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline smex restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint ivy-hydra indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-make google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist highlight evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu elisp-slime-nav dumb-jump popup f dash s diminish define-word counsel-projectile projectile pkg-info epl counsel swiper ivy column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed async aggressive-indent adaptive-wrap ace-window ace-link avy))
+ '(safe-local-variable-values
+   '((flycheck-disabled-checkers emacs-lisp-checkdoc)
+     (eval progn
+           (setq projectile-project-test-cmd #'projectile-rails-minitest-test-at-point-cmd)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+)
---
+++ spacemacs-private-layer/gusev/keybindings.el
@@ -0,0 +1,99 @@
+(cua-mode t)
+
+(global-set-key (kbd "M-<down>") 'other-window)
+(global-unset-key (kbd "C-x O"))
+(global-set-key (kbd "M-<up>") (lambda () (interactive) (other-window -1)))
+
+(global-set-key (kbd "<escape>") 'keyboard-quit)
+
+(use-package helm
+  :init
+  (setq helm-semantic-fuzzy-match t
+        helm-imenu-fuzzy-match    t
+        helm-M-x-fuzzy-match      t)
+  ;; (when (executable-find "ack-grep")
+  ;;   (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
+  ;;         helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f"))
+  ;; (when (executable-find "ag")
+  ;;   (setq helm-grep-default-command "ag --nocolor --nogroup %p %f"
+  ;;         helm-grep-default-recurse-command "ag --nocolor --nogroup %p %f"))
+  :config
+  (require 'subr-x)
+  (defvar helm-source-emacs-process
+    (helm-build-sync-source "Emacs Process"
+      :init (lambda ()
+              (let (tabulated-list-use-header-line)
+                (list-processes--refresh)))
+      :candidates (lambda () (mapcar
+                              (lambda (process)
+                                (concat (process-name process)
+                                        " ["
+                                        (string-join (process-command process) " ")
+                                        "] "))
+                              (process-list)))
+      :persistent-action (lambda (elm)
+                           (delete-process (get-process elm))
+                           (helm-delete-current-selection))
+      :persistent-help "Kill Process"
+      :action (helm-make-actions "Kill Process"
+                                 (lambda (_elm)
+                                   (cl-loop for p in (helm-marked-candidates)
+                                            do (delete-process (get-process p)))))))
+  :bind (("C-e" . helm-mini)
+         ("C-<f2>" . helm-list-emacs-process)
+         :map helm-map
+         ("<tab>" . helm-execute-persistent-action) ; rebind tab to run persistent action
+         ("C-i" . helm-execute-persistent-action) ; make TAB works in terminal
+         ("C-a" . helm-select-action) ; list actions using C-a
+         ("C-z" . undo-tree-undo)
+         :map helm-moccur-mode-map
+         ("RET" . helm-moccur-mode-goto-line-ow)
+         :map helm-find-files-map
+         ("C-z" . undo-tree-undo)
+
+))
+
+(use-package expand-region
+  :init
+  (global-set-key (kbd "C-h") 'er/expand-region)
+  (global-set-key (kbd "C-S-h") (lambda () (interactive) (er/expand-region -1))))
+
+(global-set-key (kbd "<home>") 'mwim-beginning-of-code-or-line)
+(global-set-key (kbd "C-M-l") 'indent-region)
+
+(global-set-key (kbd "C-s") (lambda () (interactive) (save-some-buffers +1)))
+
+(setq search-exit-option 'edit)
+(global-set-key (kbd "C-f") 'isearch-forward)
+(define-key isearch-mode-map "\C-f" 'isearch-repeat-forward)
+(define-key isearch-mode-map (kbd "S-<return>") 'isearch-repeat-backward)
+(define-key isearch-mode-map [return] 'isearch-repeat-forward)
+(define-key isearch-mode-map (kbd "<escape>") 'isearch-exit)
+(define-key isearch-mode-map (kbd "C-v") 'isearch-yank-kill)
+(define-key minibuffer-local-isearch-map (kbd "<escape>") 'exit-minibuffer)
+(define-key minibuffer-local-isearch-map (kbd "C-f") 'isearch-forward-exit-minibuffer)
+(define-key minibuffer-local-isearch-map (kbd "C-r") 'isearch-backward-exit-minibuffer)
+(define-key minibuffer-local-isearch-map (kbd "C-v") 'isearch-yank-kill)
+;; (remove-hook 'isearch-mode-hook 'isearch-yank-kill)
+
+(global-set-key (kbd "C-M-<down>") 'next-error)
+(global-set-key (kbd "C-M-<up>") (lambda () (interactive) (next-error -1)))
+
+(setq select-enable-clipboard t)
+(setq select-active-regions nil)
+
+(add-hook 'prog-mode-hook (lambda () (local-set-key (kbd "C-/") 'comment-dwim)))
+
+(use-package undo-tree
+  :bind (:map undo-tree-map
+              ("C-/" . nil)
+              ("C-S-z" . undo-tree-redo))) 
+
+(global-set-key [f6] 'toggle-truncate-lines)
+(use-package winner
+  :init
+  (global-set-key [f2] 'winner-undo)
+  (global-set-key [f3] 'winner-redo))
+
+(global-set-key (kbd "C-j") '(lambda () (interactive) (next-line) (join-line)))
+
