564e14ca281dfcca3df84d5e0af099e5e5e15252
Aleksei Gusev <aleksei.gusev@gmail.com>
2020-07-04 10:34:00 +0300

    Some improvements for doom emacs configs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -172,6 +172,9 @@
 
 (global-set-key (kbd "<escape>") 'keyboard-quit)
 
+(global-set-key (kbd "M-[") 'previous-buffer)
+(global-set-key (kbd "M-]") 'next-buffer)
+
 (use-package! helm
   :init
   (setq helm-semantic-fuzzy-match t
@@ -205,6 +208,8 @@
                                  (lambda (_elm)
                                    (cl-loop for p in (helm-marked-candidates)
                                             do (delete-process (get-process p)))))))
+  (setq helm-mini-default-sources
+        '(helm-source-buffers-list helm-source-recentf helm-source-projectile-files-list))
   :bind (("C-e" . helm-mini)
          ("C-<f2>" . helm-list-emacs-process)
          :map helm-map
@@ -302,16 +307,54 @@
          ("M-r" . jest-test-rerun-test)
          ("M-R" . jest-test-debug-rerun-test)))
 
+;; (set-popup-rule! "\\*compilation" :side 'right :width 0.5 :modeline t)
+;; (plist-put +popup-defaults :modeline t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#f0f0f0" "#e45649" "#50a14f" "#986801" "#4078f2" "#a626a4" "#0184bc" "#383a42"])
+ '(custom-safe-themes
+   (quote
+    ("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" default)))
+ '(fci-rule-color "#383a42")
+ '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#4078f2"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#50a14f"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#9ca0a4"))
+ '(objed-cursor-color "#e45649")
+ '(pdf-view-midnight-colors (cons "#383a42" "#fafafa"))
+ '(rustic-ansi-faces
+   ["#fafafa" "#e45649" "#50a14f" "#986801" "#4078f2" "#a626a4" "#0184bc" "#383a42"])
  '(safe-local-variable-values
    (quote
     ((js2-strict-missing-semi-warning)
      (js2-basic-offset . 2))))
- '(sp-override-key-bindings (quote (("C-<right>") ("C-<left>")))))
+ '(sp-override-key-bindings (quote (("C-<right>") ("C-<left>"))))
+ '(vc-annotate-background "#fafafa")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#50a14f")
+    (cons 40 "#688e35")
+    (cons 60 "#807b1b")
+    (cons 80 "#986801")
+    (cons 100 "#ae7118")
+    (cons 120 "#c37b30")
+    (cons 140 "#da8548")
+    (cons 160 "#c86566")
+    (cons 180 "#b74585")
+    (cons 200 "#a626a4")
+    (cons 220 "#ba3685")
+    (cons 240 "#cf4667")
+    (cons 260 "#e45649")
+    (cons 280 "#d2685f")
+    (cons 300 "#c07b76")
+    (cons 320 "#ae8d8d")
+    (cons 340 "#383a42")
+    (cons 360 "#383a42")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
