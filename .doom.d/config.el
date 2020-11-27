9993604749d85454348e831f0c3adb24d7b4a4e8
Aleksei Gusev <aleksei.gusev@f-secure.com>
2020-11-27 10:06:07 +0200

    Enable global-subword-mode

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -54,7 +54,8 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(cua-mode)
+(cua-mode +1)
+(global-subword-mode +1)
 
 (use-package! smartparens
   :config
