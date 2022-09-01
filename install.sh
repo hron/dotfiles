d7e3b105a4b50c619664e48536177dd2bb7bd20c
Aleksei Gusev <aleksei.gusev@gmail.com>
2022-09-01 18:02:10 +0300

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+set -x
+
+mkdir -p $HOME/bin;
+
+rm $HOME/.bashrc
+
+for each in $(find bin/ -mindepth 1 | egrep -v '#$|~$') $( ls -a1 | egrep -v '(^install.sh$|^bin$|\.git$|\.gitignore$|\.gitmodules|^\.\.?$|\~$|#$)'); do
+    [ -e $HOME/$each ] || ln -nfs `pwd`/$each $HOME/$each;
+done
+
