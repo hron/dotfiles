c5fdbba752a5b52674d662647da593a582c4bb50
Aleksei Gusev <aleksei.gusev@withsecure.com>
2022-10-22 13:57:10 +0300

    emacs: Some random custom.el changes

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,18 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "70b596389eac21ab7f6f7eb1cf60f8e60ad7c34ead1f0244a577b1810e87e58c" "c865644bfc16c7a43e847828139b74d1117a6077a845d16e71da38c8413a5aaa" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "0c08a5c3c2a72e3ca806a29302ef942335292a80c2934c1123e8c732bb2ddd77" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" default))
  '(safe-local-variable-values
-   '((eval setq ein:jupyter-server-command
-      (concat
-       (projectile-project-root)
-       "/venv/bin/jupyter"))
-     (eval progn
-      (setq dap-python-executable
-            (concat
-             (projectile-project-root)
-             "/venv/bin/python")))))
+   '((eval progn
+           (setq dap-python-executable
+                 (concat
+                  (projectile-project-root)
+                  "/venv/bin/python")))))
  '(sp-override-key-bindings '(("C-<right>") ("C-<left>"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
