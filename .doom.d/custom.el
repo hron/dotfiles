3d0a3d9c8e3cf662cd3a7dfaed30fbd35a31690d
Aleksei Gusev <aleksei.gusev@withsecure.com>
2022-10-25 11:24:09 +0300

    emacs: custom.el

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((toc-org-max-depth . 4)
+   '((checkdoc-package-keywords-flag)
+     (toc-org-max-depth . 4)
      (eval progn
       (setq dap-python-executable
             (concat
