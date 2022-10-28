8ffff169d0b107a467478c5e6d979fbcdd55eea1
Aleksei Gusev <aleksei.gusev@withsecure.com>
2022-10-28 09:04:42 +0300

    emacs: custom.el

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((eval setq ein:jupyter-server-command
+   '((lsp-eslint-experimental-incremental-sync)
+     (lsp-eslint-package-manager . "yarn")
+     (eval setq ein:jupyter-server-command
       (concat
        (projectile-project-root)
        "/venv/bin/jupyter"))
