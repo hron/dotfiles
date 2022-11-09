4d635edefdeb9ff71bc859885fcca9753b081465
Aleksei Gusev <aleksei.gusev@withsecure.com>
2022-11-09 21:07:23 +0200

    emacs: Better line spacing

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -56,6 +56,10 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+(use-package! emacs
+  :config
+  (setq-default line-spacing 3))
+
 (map! "C-<f2>" 'list-processes)
 
 (use-package! iflipb
