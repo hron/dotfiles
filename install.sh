7757171e180dc01be46dfe8c648dedc00c8e62e6
Aleksei Gusev <aleksei.gusev@withsecure.com>
2022-11-20 16:00:29 +0200

    Add Astro Nvim config

--- install.sh
+++ install.sh
@@ -5,9 +5,13 @@ set -e
 mkdir -p $HOME/bin;
 
 rm -f $HOME/.bashrc
-for each in $(find bin/ -mindepth 1 | egrep -v '#$|~$') $( ls -a1 | egrep -v '(^install.sh$|^bin$|\.git$|\.gitignore$|\.gitmodules|^\.\.?$|\~$|#$)'); do
+for each in $(find bin/ -mindepth 1 | egrep -v '#$|~$') $( ls -a1 | egrep -v '(^AstoNvim$|^install.sh$|^bin$|\.git$|\.gitignore$|\.gitmodules|^\.\.?$|\~$|#$)'); do
     [ -e $HOME/$each ] || ln -nfs `pwd`/$each $HOME/$each;
 done
+
+astro_nvim_user_dir=$HOME/.config/nvim/lua/user
+[ -e $HOME/.config/nvim ] || ln -nfs `pwd`/AstroNvim $HOME/.config/nvim
+[ -e $astro_nvim_user_dir ] || mkdir astro_nvim_user_dir && ln -nfs `pwd`/.nvim-astro $astro_nvim_user_dir
 
 if grep -v starship $HOME/.profile; then
     curl -sS https://starship.rs/install.sh > $HOME/bin/install-starship.sh
