8243efec202a9957a4165d569b21727c75982985
Aleksei Gusev <aleksei.gusev@withsecure.com>
2022-11-21 20:11:01 +0200

    emacs: Use bold font

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,9 +21,13 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "CaskaydiaCove Nerd Font" :size 18 :weight 'semi-bold :slant 'normal)
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 18 :weight 'bold :slant 'normal)
       doom-unicode-font doom-font
       doom-variable-pitch-font (font-spec :family "sans"))
+
+(use-package! emacs
+  :custom-face
+  (bold ((t (:weight extra-bold)))))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
