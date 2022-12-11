221df887a4fe09aa55ab453fc8b7f5f06b397192
Aleksei Gusev <aleksei.gusev@gmail.com>
2022-12-11 10:26:03 +0200

    emas: custom.el

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("9e7119d21b31e80bc7c7ccee937cd687bfc1617b61b413e2ba1683df0fb10248" default))
  '(safe-local-variable-values
    '((eval progn
       (setq dap-python-executable
