f1e091ca34542463bd17ca63656bf846d6ab5b43
Aleksei Gusev <aleksei.gusev@withsecure.com>
2023-01-19 15:35:44 +0200

    emacs: Avoid posframe for flycheck

--- .doom.d/configs/flycheck.el
+++ .doom.d/configs/flycheck.el
@@ -7,9 +7,10 @@
                           (flycheck-list-errors)
                           (select-window (get-buffer-window flycheck-error-list-buffer)))))
   :config
-  (setq flycheck-display-errors-delay 60
-        flycheck-checker-error-threshold 999))
+  (setq flycheck-display-errors-delay 0.9
+        flycheck-checker-error-threshold 999)
+  (remove-hook 'flycheck-mode-hook #'+syntax-init-popups-h))
 
-(use-package! flycheck-posframe
-  :custom
-  (flycheck-posframe-border-width 1))
+;; (use-package! flycheck-posframe
+;;   :custom
+;;   (flycheck-posframe-border-width 1))
