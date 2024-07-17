89f2b8cbadda60405442c1348b542bfb3bd4f80f
Aleksei Gusev <aleksei.gusev@gmail.com>
2024-07-17 14:36:39 +0300

    Add C-S-m shortcut

--- .doom.d/configs/flycheck.el
+++ .doom.d/configs/flycheck.el
@@ -2,10 +2,10 @@
   :bind (:map flycheck-mode-map
               ("<f2>" . #'flycheck-next-error)
               ("S-<f2>" . #'flycheck-previous-error)
-              ("C-6" . (lambda ()
-                          (interactive)
-                          (flycheck-list-errors)
-                          (select-window (get-buffer-window flycheck-error-list-buffer)))))
+              ("S-C-m" . (lambda ()
+                           (interactive)
+                           (flycheck-list-errors)
+                           (select-window (get-buffer-window flycheck-error-list-buffer)))))
   :config
   (setq flycheck-display-errors-delay 0.9
         flycheck-checker-error-threshold 999)
