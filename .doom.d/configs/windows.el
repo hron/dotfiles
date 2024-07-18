f9893c5d36e1e7745ceb5da8f208bc58e48b7780
Aleksei Gusev <aleksei.gusev@gmail.com>
2024-07-18 13:21:08 +0300

    Remove popper.el

--- .doom.d/configs/windows.el
+++ .doom.d/configs/windows.el
@@ -18,27 +18,30 @@
     (select-window window)  ;; Select the window displaying the buffer
     window))
 
-(defun aleksei/popper-display-popup-at-bottom-or-right (buffer &optional alist)
-  "Display popup-buffer BUFFER at the bottom of the screen."
-  (let ((wide-frame-opts '((window-width . .5)
-                           (side . right)
-                           (slot . 1)))
-        (narrow-frame-opts '((window-height . 0.33)
-                             (side . bottom)
-                             (slot . 1))))
+(defun aleksei/display-buffer-in-side-window (buffer &optional alist)
+  "Display BUFFER at the appropriate place depending on the current frame width"
+  (let* ((side-width (or (cdr (assq 'side-width alist)) .5))
+         (side-height (or (cdr (assq 'side-height alist)) .33))
+         (wide-frame-opts `(list
+                            (window-width . ,side-width)
+                            (side . left)))
+         (narrow-frame-opts `(list
+                              (window-height . ,side-height)
+                              (side . bottom))))
 
     (display-buffer-in-side-window
      buffer
      (append alist
-             (if (> (frame-width) 200) wide-frame-opts narrow-frame-opts)))))
+             (if (> (frame-width) 200) wide-frame-opts narrow-frame-opts))
+     )))
 
-(defun aleksei/other-popper-buffer-p (buffer)
-  "Predicate to detect if BUFFER is a popup"
-  (let ((buffer (if (bufferp buffer) buf (get-buffer buffer))))
-    (with-current-buffer buffer
-      (or (memq popper-popup-status '(popup user-popup))
-          (unless (eq popper-popup-status 'raised)
-            (popper-popup-p buffer))))))
+;; (defun aleksei/other-popper-buffer-p (buffer)
+;;   "Predicate to detect if BUFFER is a popup"
+;;   (let ((buffer (if (bufferp buffer) buf (get-buffer buffer))))
+;;     (with-current-buffer buffer
+;;       (or (memq popper-popup-status '(popup user-popup))
+;;           (unless (eq popper-popup-status 'raised)
+;;             (popper-popup-p buffer))))))
 
 (defun aleksei/3-columns-layout-p ()
   (and (>= (frame-width) 305)))
@@ -52,123 +55,126 @@
 
 (setq display-buffer-alist
       '(
-        ("\\*Flycheck error"
-         (display-buffer-in-side-window)
-         (window-height . popper--fit-window-height)
-         (side . bottom)
-         (slot . 1))
-
-        ;; ("\\*lsp-help"
-        ;;  (display-buffer-in-side-window)
-        ;;  (window-height . popper--fit-window-height)
-        ;;  (side . bottom)
-        ;;  (slot . 1)
-        ;;  (select . t))
-
-        ("\\*git-gutter:diff"
-         (display-buffer-in-side-window)
-         (window-height . popper--fit-window-height)
-         (side . bottom)
-         (slot . 1))
-
-        ((lambda (buff &optional alist)
-           (and (string-match-p "magit-revision" buff)
-                (aleksei/1-column-layout-p)))
-         (display-buffer-in-side-window)
-         (side . bottom)
-         (window-height . 0.67)
-         (slot . 1)
-         (select . t))
-
-        ((derived-mode . process-menu-mode)
-         (display-buffer-reuse-mode-window display-buffer-at-bottom-and-fit)
-         (side . bottom)
-         (window-height . fit-window-to-buffer)
-         (preserve-size . (nil . t))
-         (window-parameters . ((inhibit-same-window . nil)
-                               (select-window . t)))
+        (
+         ;; (or . ("\\*Warnings\\*"
+         ;;        "\\*projectile-files-errors\\*"))
+         ;; (display-buffer-no-window)
+         ;; (allow-no-window . t)
          )
 
-        ;; ((derived-mode . process-menu-mode)
-        ;;  (display-buffer-reuse-mode-window display-buffer-below-selected)
-        ;;  (dedicated . t)
-        ;;  (select . t)
-        ;;  (window-height . fit-window-to-buffer)
-        ;;  (window-parameters . ((select-window . t))))
+        ("magit-revision"
+         (aleksei/display-buffer-in-side-window)
+         (side-width . .55)
+         (side-height . .67))
 
-        ("\\*Warnings\\*"
-         (display-buffer-no-windol))
-
-        ((lambda (buff &optional alist) (and (aleksei/other-popper-buffer-p buff) (aleksei/3-columns-layout-p)))
-         (display-buffer-in-side-window)
-         (window-width . 0.33)
-         (side . left)
-         (slot . 1))
-
-        ((lambda (buff &optional alist) (and (aleksei/other-popper-buffer-p buff) (aleksei/2-columns-layout-p)))
-         (display-buffer-in-side-window)
-         (window-width . 0.5)
-         (side . left)
-         (slot . 1))
-
-        ((lambda (buff &optional alist) (and (aleksei/other-popper-buffer-p buff) (aleksei/1-column-layout-p)))
-         (display-buffer-in-side-window)
-         (window-height . 0.33)
-         (side . bottom)
-         (slot . 1))
-
-        (".*" (display-buffer-reuse-mode-window))
+        ((or . ("^\\*"))
+         (aleksei/display-buffer-in-side-window)
+         (side-width . .45)
+         (side-height . .33))
         ))
 
-(use-package popper
-  :bind (("C-M-`"   . popper-toggle-latest)
-         ("M-`"   . popper-cycle)
-         ("C-~" . popper-toggle-type))
-  :custom
-  (popper-display-control 'user)
-  (popper-display-function #'aleksei/popper-display-popup-at-bottom-or-right)
-  (popper-group-function #'popper-group-by-projectile)
-  (popper-reference-buffers
-   '("\\*Messages\\*"
-     "\\*Warnings\\*"
-     "Output\\*$"
-     "\\*Async Shell Command\\*"
-     "\\*doom eval\\*"
-     "\\*doom:scratch"
-     "\\*ert\\*"
-     "\\*git-gutter:diff"
-     "\\*SQL: "
-     "\\*Flycheck error"
-     "\\*Apropos"
-     "\\*lsp-help"
-     "\\*lsp-ui-imenu"
+;; (setq display-buffer-alist
+;;       '(
+;;         ("\\*Flycheck error"
+;;          (display-buffer-in-side-window)
+;;          (window-height . popper--fit-window-height)
+;;          (side . bottom)
+;;          (slot . 1))
+;;
+;;         ("\\*git-gutter:diff"
+;;          (display-buffer-in-side-window)
+;;          (window-height . popper--fit-window-height)
+;;          (side . bottom)
+;;          (slot . 1))
+;;
+;;         ((lambda (buff &optional alist)
+;;            (and (string-match-p "magit-revision" buff)
+;;                 (aleksei/1-column-layout-p)))
+;;          (display-buffer-in-side-window)
+;;          (side . bottom)
+;;          (window-height . 0.67)
+;;          (slot . 1)
+;;          (select . t))
+;;
+;;         ((derived-mode . process-menu-mode)
+;;          (display-buffer-reuse-mode-window display-buffer-at-bottom-and-fit)
+;;          (side . bottom)
+;;          (window-height . fit-window-to-buffer)
+;;          (preserve-size . (nil . t))
+;;          (window-parameters . ((inhibit-same-window . nil)
+;;                                (select-window . t)))
+;;          )
+;;
+;;         ((lambda (buff &optional alist) (and (aleksei/other-popper-buffer-p buff) (aleksei/3-columns-layout-p)))
+;;          (display-buffer-in-side-window)
+;;          (window-width . 0.33)
+;;          (side . left)
+;;          (slot . 1))
+;;
+;;         ((lambda (buff &optional alist) (and (aleksei/other-popper-buffer-p buff) (aleksei/2-columns-layout-p)))
+;;          (display-buffer-in-side-window)
+;;          (window-width . 0.5)
+;;          (side . left)
+;;          (slot . 1))
+;;
+;;         ((lambda (buff &optional alist) (and (aleksei/other-popper-buffer-p buff) (aleksei/1-column-layout-p)))
+;;          (display-buffer-in-side-window)
+;;          (window-height . 0.33)
+;;          (side . bottom)
+;;          (slot . 1))
+;;
+;;         (".*" (display-buffer-reuse-mode-window))
+;;         ))
 
-     helpful-mode help-mode
-
-     compilation-mode comint-mode "\\*mocha"
-
-     "\\*tree-sitter-tree:"
-
-     "\\*cargo"
-
-     "\\*rg"
-
-     "\\*Embark"
-
-     "\\*format-all-errors\\*"
-
-     "^\\*eshell.*\\*$" eshell-mode ;eshell as a popup
-     "^\\*shell.*\\*$"  shell-mode  ;shell as a popup
-     "^\\*term.*\\*$"   term-mode   ;term as a popup
-     "^\\*vterm.*\\*$"  vterm-mode  ;vterm as a popup
-
-     grep-mode
-
-     "\\*projectile-files-errors\\*"
-
-     ;; magit-revision-mode
-     ))
-  (popper-mode-line "")
-  :init
-  (popper-mode -1)
-  (popper-echo-mode +1))
+;; (use-package popper
+;;   :bind (("C-M-`"   . popper-toggle-latest)
+;;          ("M-`"   . popper-cycle)
+;;          ("C-~" . popper-toggle-type))
+;;   :custom
+;;   (popper-display-control 'user)
+;;   (popper-display-function #'aleksei/popper-display-popup-at-bottom-or-right)
+;;   (popper-group-function #'popper-group-by-projectile)
+;;   (popper-reference-buffers
+;;    '("\\*Messages\\*"
+;;      "\\*Warnings\\*"
+;;      "Output\\*$"
+;;      "\\*Async Shell Command\\*"
+;;      "\\*doom eval\\*"
+;;      "\\*doom:scratch"
+;;      "\\*ert\\*"
+;;      "\\*git-gutter:diff"
+;;      "\\*SQL: "
+;;      "\\*Flycheck error"
+;;      "\\*Apropos"
+;;      "\\*lsp-help"
+;;      "\\*lsp-ui-imenu"
+;;
+;;      helpful-mode help-mode
+;;
+;;      compilation-mode comint-mode "\\*mocha"
+;;
+;;      "\\*tree-sitter-tree:"
+;;
+;;      "\\*cargo"
+;;
+;;      "\\*rg"
+;;
+;;      "\\*Embark"
+;;
+;;      "\\*format-all-errors\\*"
+;;
+;;      "^\\*eshell.*\\*$" eshell-mode ;eshell as a popup
+;;      "^\\*shell.*\\*$"  shell-mode  ;shell as a popup
+;;      "^\\*term.*\\*$"   term-mode   ;term as a popup
+;;      "^\\*vterm.*\\*$"  vterm-mode  ;vterm as a popup
+;;
+;;      grep-mode
+;;
+;;      "\\*projectile-files-errors\\*"
+;;
+;;      ;; magit-revision-mode
+;;      ))
+;;   (popper-mode-line "")
+;;   :init
+;;   (popper-mode -1)
+;;   (popper-echo-mode +1))
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -64,7 +64,7 @@
 
 (package! dpkg-dev-el)
 
-(package! popper :pin "d7560f18350faaee8362aee16481268de3cc6457")
+;; (package! popper :pin "d7560f18350faaee8362aee16481268de3cc6457")
 
 (package! string-inflection :pin "50ad54970b3cc79b6b83979bde9889ad9a9e1a9c")
 
