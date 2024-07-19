2024ea9d735a914e9273f243984a1459decd9e37
Aleksei Gusev <aleksei.gusev@gmail.com>
2024-07-19 10:54:22 +0300

    Some more Emacs tweaks

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -22,14 +22,13 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 (defun aleksei/font-size ()
-  "Returns font size depending on the environment. Currently I use a smaller font on Wayland"
-  13)
-
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size (aleksei/font-size) :weight 'medium)
-      doom-unicode-font doom-font
-      doom-variable-pitch-font (font-spec :family "sans" :size (aleksei/font-size)))
-
-
+  "Returns font size depending on the environment. Currently I use a smaller font on Wayland"
+  13)
+
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size (aleksei/font-size) :weight 'medium)
+      doom-unicode-font doom-font
+      doom-variable-pitch-font (font-spec :family "sans" :size (aleksei/font-size)))
+
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
