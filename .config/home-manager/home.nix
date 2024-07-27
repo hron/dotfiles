f4b7bed7a3fb334d497980d5ae580565fbadf91a
Aleksei Gusev <aleksei.gusev@gmail.com>
2024-07-27 14:29:43 +0300

    Use Emacs from Nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,13 +1,15 @@
 { config, pkgs, ... }:
 
 let
-  treesitGrammars =
-    (pkgs.emacsPackagesFor pkgs.emacs29-pgtk).treesit-grammars.with-all-grammars;
-  emacs = (pkgs.emacsPackagesFor pkgs.emacs29-pgtk).emacsWithPackages (epkgs: with epkgs; [
-    vterm
-    treesitGrammars
-  ]);
-in {
+  treesitGrammars = (pkgs.emacsPackagesFor pkgs.emacs29-pgtk).treesit-grammars.with-all-grammars;
+  emacs = (pkgs.emacsPackagesFor pkgs.emacs29-pgtk).emacsWithPackages (
+    epkgs: with epkgs; [
+      vterm
+      treesitGrammars
+    ]
+  );
+in
+{
   home.username = "algus";
   home.homeDirectory = "/home/algus";
 
@@ -22,10 +24,7 @@ in {
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    emacs
-  ];
-
+  home.packages = with pkgs; [ nixfmt-rfc-style ] ++ [ emacs ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -64,4 +63,76 @@ in {
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  #
+  # https://github.com/nix-community/home-manager/issues/1439
+  home.activation = {
+    linkDesktopApplications = {
+      after = [
+        "writeBoundary"
+        "createXdgUserDirectories"
+      ];
+      before = [ ];
+      data = ''
+        rm -rf ${config.xdg.dataHome}/"applications/home-manager"
+        mkdir -p ${config.xdg.dataHome}/"applications/home-manager"
+        cp -Lr ${config.home.homeDirectory}/.nix-profile/share/applications/* ${config.xdg.dataHome}/"applications/home-manager/"
+      '';
+    };
+  };
+
+  xdg.enable = true;
+  xdg.mime.enable = true;
+
+  xdg.desktopEntries = {
+    # We need to redefine StartupWMClass, otherwise the desktop entry is the same
+    emacs = {
+      name = "Emacs";
+      genericName = "Text Editor";
+      comment = "Edit text";
+      mimeType = [
+        "text/english"
+        "text/plain"
+        "text/x-makefile"
+        "text/x-c++hdr"
+        "text/x-c++src"
+        "text/x-chdr"
+        "text/x-csrc"
+        "text/x-java"
+        "text/x-moc"
+        "text/x-pascal"
+        "text/x-tcl"
+        "text/x-tex"
+        "application/x-shellscript"
+        "text/x-c"
+        "text/x-c++"
+      ];
+      exec = "emacs %F";
+      icon = "emacs";
+      type = "Application";
+      terminal = false;
+      categories = [
+        "Development"
+        "TextEditor"
+      ];
+      startupNotify = true;
+      settings = {
+        StartupWMClass = "emacs";
+      };
+    };
+
+    orgMode = {
+      name = "GTD";
+      comment = "Get Things Done with org-mode";
+      exec = "emacs --name org-mode --eval \"(aleksei/org-gtd)\"";
+      icon = "org.gnome.Todo";
+      type = "Application";
+      terminal = false;
+      categories = [ "Utility" ];
+      startupNotify = true;
+      settings = {
+        StartupWMClass = "org-mode";
+      };
+    };
+  };
 }
