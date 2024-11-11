d7804412b4af987c2d10b05b63d0da5b231b87ea
Aleksei Gusev <aleksei.gusev@gmail.com>
2024-11-12 00:27:09 +0200

    Add window rules for Discord

--- .config/devilspie2/rules.lua
+++ .config/devilspie2/rules.lua
@@ -2,6 +2,9 @@ debug_print("Application: " .. get_application_name())
 debug_print("Window: " .. get_window_name());
 
 if get_application_name() == "Loading..." or get_application_name() == "YouTube Music" then
-    debug_print("pinning!!")
     pin_window()
+end
+
+if string.match(get_application_name(), "- Discord$") then
+    set_window_geometry(721, 215, 2399, 1651)
 end
