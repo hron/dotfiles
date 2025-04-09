25d13eecdc6ba8f07066290ddf39d734a918bbad
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-04-09 13:55:02 +0300

    Setup new Vanilla Emacs configuration

---
+++ .config/emacs/early-init.el
@@ -0,0 +1,27 @@
+;;; early-init.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Aleksei Gusev
+;;
+;; Author: Aleksei Gusev <aleksei.gusev@gmail.com>
+;; Maintainer: Aleksei Gusev <aleksei.gusev@gmail.com>
+;; Created: April 09, 2025
+;; Modified: April 09, 2025
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc wp
+;; Homepage: https://github.com/hron/early-init
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(add-to-list 'default-frame-alist
+             '(font . "JetBrainsMono Nerd Font-10"))
+
+
+(provide 'early-init)
+;;; early-init.el ends here
---
+++ .config/emacs/init.el
@@ -0,0 +1,55 @@
+;;; init.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Aleksei Gusev
+;;
+;; Author: Aleksei Gusev <aleksei.gusev@gmail.com>
+;; Maintainer: Aleksei Gusev <aleksei.gusev@gmail.com>
+;; Created: April 09, 2025
+;; Modified: April 09, 2025
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc wp
+;; Homepage: https://github.com/hron/dotfiles/.config/emacs
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+;; straight.el bootstrap
+(setq straight-use-package-by-default t)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Aleksei Gusev"
+      user-mail-address "aleksei.gusev@gmail.com")
+
+(use-package gptel
+  :bind (:map global-map
+              ("C-<return>" . gptel-menu)
+              ;; :map gptel-mode
+              ;; ("C-<return>" . gptel-send)
+              )
+  :config (setq gptel-model "gpt-4o"))
+
+(provide 'init)
+;;; init.el ends here
