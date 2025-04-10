f6e1b7e23df397f3c954c97c799ad22aa6b6060c
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-04-10 13:28:43 +0300

    Setup search and replace in Vanilla Emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -35,9 +35,60 @@
   (cua-rectangle-mark-key [(control shift return)]))
 
 (use-package undo-fu
-  :bind (:map global-map
-              ("C-z"   . undo-fu-only-undo)
-              ("C-S-z" . undo-fu-only-redo)))
+  :bind (("C-z"   . undo-fu-only-undo)
+         ("C-S-z" . undo-fu-only-redo)))
+
+(use-package emacs
+  :init
+  (defun algus/save-all-buffers ()
+    "Saves all modified buffers, literally (save-some-buffers +1)"
+    (interactive)
+    (save-some-buffers +1))
+  :bind (("C-<f2>" . list-processes)
+         ("C-d" . duplicate-dwim)
+         ("C-M-l" . indent-region)
+         ("C-s" . algus/save-all-buffers)))
+
+(setq-default cursor-type '(bar . 3))
+(modify-all-frames-parameters
+ '((font . "JetBrainsMono Nerd Font-10")))
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(fringe-mode (frame-char-width))
+
+(use-package drag-stuff
+  :bind (("M-<up>" . nil)
+         ("M-<down>" . nil)
+         ("M-S-<up>" . #'drag-stuff-up)
+         ("M-S-<down>" . #'drag-stuff-down)))
+
+(use-package anzu
+  :init
+  ;;;###autoload
+  (defun aleksei/isearch-region-or-forward ()
+    "Do incremental search forward, use region if it's active"
+    (interactive)
+    (if (use-region-p)
+        (isearch-forward-thing-at-point)
+      (isearch-forward)))
+  (remove-hook 'isearch-mode-hook 'isearch-yank-kill)
+  (global-anzu-mode +1)
+
+  :bind (("C-f" . #'aleksei/isearch-region-or-forward)
+         :map isearch-mode-map
+         ("C-f" . #'isearch-repeat-forward)
+         ("C-r" . #'anzu-query-replace-regexp)
+         ("S-<return>" . #'isearch-repeat-backward)
+         ("<return>" . #'isearch-repeat-forward)
+         ("C-g" . #'isearch-exit)
+         ("C-v" . #'isearch-yank-kill)
+         :map minibuffer-local-isearch-map
+         ("C-f" . #'isearch-forward-exit-minibuffer)
+         ("C-r" . #'isearch-backward-exit-minibuffer)
+         ("C-v" . #'isearch-yank-kill))
+
+  :custom (search-exit-option . 'edit))
 
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
@@ -56,25 +107,29 @@
               )
   :config (setq gptel-model "gpt-4o"))
 
-(setq-default cursor-type '(bar . 3))
-(modify-all-frames-parameters
- '((font . "JetBrainsMono Nerd Font-10")))
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(fringe-mode (frame-char-width))
-
-(use-package emacs
-  :bind (:map global-map
-              ("C-<f2>" . list-processes)
-              ("C-d" . duplicate-dwim)))
-
 (use-package magit
   :bind (:map magit-section-mode-map
          ("C-<tab>" . nil)
          ("<C-iso-lefttab>" . nil)
          :map magit-mode-map
          ("C-w" . delete-window)))
+
+;; emacs-lisp-mode
+(use-package emacs
+  :bind (:map emacs-lisp-mode-map
+              ("C-q" . describe-symbol))
+  :hook ((emacs-lisp-mode . (lambda () (setq tab-width 2)))))
+
+(use-package ert
+  :bind (:map emacs-lisp-mode-map
+              ("C-; f" . ert)))
+
+(use-package expand-region
+  :bind (("C-h" . er/expand-region)
+         ("C-S-h" . er/contract-region)))
+
+(use-package crux
+  :bind (("<home>" . crux-move-beginning-of-line)))
 
 (require 'algus-org)
 
