81c1e131e78ad5592fb6e45f800823b2125a37a8
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-04-16 16:35:54 +0300

    Adopt desktop-save-mode

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -821,37 +821,44 @@
 
 ;;; desktop-save-mode configuration
 
-;; (define-minor-mode +lsp-optimization-mode
-;;   "Deploys universal GC and IPC optimizations for `lsp-mode' and `eglot'."
-;;   :global t
-;;   :init-value nil
-;;   (require 'gcmh)
-;;   (if (not +lsp-optimization-mode)
-;;       (setq-default read-process-output-max +lsp--default-read-process-output-max
-;;                     gcmh-high-cons-threshold +lsp--default-gcmh-high-cons-threshold
-;;                     +lsp--optimization-init-p nil)
-;;     ;; Only apply these settings once!
-;;     (unless +lsp--optimization-init-p
-;;       (setq +lsp--default-read-process-output-max (default-value 'read-process-output-max)
-;;             +lsp--default-gcmh-high-cons-threshold (default-value 'gcmh-high-cons-threshold))
-;;       (setq-default read-process-output-max (* 1024 1024))
-;;       ;; REVIEW LSP causes a lot of allocations, with or without the native JSON
-;;       ;;        library, so we up the GC threshold to stave off GC-induced
-;;       ;;        slowdowns/freezes. Doom uses `gcmh' to enforce its GC strategy,
-;;       ;;        so we modify its variables rather than `gc-cons-threshold'
-;;       ;;        directly.
-;;       (setq-default gcmh-high-cons-threshold (* 2 +lsp--default-gcmh-high-cons-threshold))
-;;       (gcmh-set-high-threshold)
-;;       (setq +lsp--optimization-init-p t))))
+(define-minor-mode +lsp-optimization-mode
+  "Deploys universal GC and IPC optimizations for `lsp-mode' and `eglot'."
+  :global t
+  :init-value nil
+  (require 'gcmh)
+  (if (not +lsp-optimization-mode)
+      (setq-default read-process-output-max +lsp--default-read-process-output-max
+                    gcmh-high-cons-threshold +lsp--default-gcmh-high-cons-threshold
+                    +lsp--optimization-init-p nil)
+    ;; Only apply these settings once!
+    (unless +lsp--optimization-init-p
+      (setq +lsp--default-read-process-output-max (default-value 'read-process-output-max)
+            +lsp--default-gcmh-high-cons-threshold (default-value 'gcmh-high-cons-threshold))
+      (setq-default read-process-output-max (* 1024 1024))
+      ;; REVIEW LSP causes a lot of allocations, with or without the native JSON
+      ;;        library, so we up the GC threshold to stave off GC-induced
+      ;;        slowdowns/freezes. Doom uses `gcmh' to enforce its GC strategy,
+      ;;        so we modify its variables rather than `gc-cons-threshold'
+      ;;        directly.
+      (setq-default gcmh-high-cons-threshold (* 2 +lsp--default-gcmh-high-cons-threshold))
+      (gcmh-set-high-threshold)
+      (setq +lsp--optimization-init-p t))))
 
-;; (add-hook! 'doom-project-hook '(lambda (&rest _)
-;;                                  (when-let ((root (doom-project-root)))
-;;                                    (when (not (string= desktop-dirname root))
-;;                                      (desktop-change-dir root)))))
-;; (use-package! emacs
-;;   :init
-;;   (desktop-save-mode +1)
-;;   :custom
-;;   (desktop-path (list "." user-emacs-directory)))
+(add-hook! 'doom-project-hook '(lambda (&rest _)
+                                 (when-let ((root (doom-project-root)))
+                                   (when (not (string= desktop-dirname root))
+                                     (desktop-change-dir root)))))
+
+(use-package! emacs
+  :init
+  ;; This hook incorrectly enables whitespace-mode after desktop is restored.
+  (add-hook 'window-setup-hook
+            '(lambda ()
+               (remove-hook 'after-change-major-mode-hook #'doom-highlight-non-default-indentation-h))
+            100)
+
+  (desktop-save-mode +1)
+  :custom
+  (desktop-path (list "." user-emacs-directory)))
 
 (add-hook! typescript-ts-mode-local-vars :append #'+javascript-init-lsp-or-tide-maybe-h)
