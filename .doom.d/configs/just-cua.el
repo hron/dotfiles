4fba0768e50652c8510bc47975e194a10ac0d7d3
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-04-26 09:40:47 +0300

    Restore ESC in Emacs

--- .doom.d/configs/just-cua.el
+++ .doom.d/configs/just-cua.el
@@ -8,13 +8,11 @@
   :config
   (cua-mode +1))
 
-;; (defun aleksei/define-global-key-translations (&optional frame)
-;;   "ESC according modern conventions"
-;;   (with-selected-frame (or frame (selected-frame))
-;;     ;; C-x
-;;     ;; Escape
-;;     (define-key key-translation-map (kbd "ESC") (kbd "C-g"))))
+(defun aleksei/define-global-key-translations (&optional frame)
+  "ESC according modern conventions"
+  (with-selected-frame (or frame (selected-frame))
+    ;; Escape
+    (define-key key-translation-map (kbd "ESC") (kbd "C-g"))))
 
-;; (aleksei/define-global-key-translations)
-;; (add-hook 'after-make-frame-functions 'aleksei/define-global-key-translations)
-;; (global-unset-key (kbd "C-<return>"))
+(aleksei/define-global-key-translations)
+(add-hook 'after-make-frame-functions 'aleksei/define-global-key-translations)
