a3e94f80cd3d545527fedacb7c0350b3137135b3
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-04-26 10:47:40 +0300

    Disable all display-buffer related configurations

--- .doom.d/configs/windows.el
+++ .doom.d/configs/windows.el
@@ -7,77 +7,77 @@
          ("C-<prior>" . (lambda () (interactive) (other-window -1)))
          ("M-i" . delete-other-windows)))
 
-(setq aleksei/default-side-window-width .5
-      aleksei/default-side-window-height .33)
+;; (setq aleksei/default-side-window-width .5
+;;       aleksei/default-side-window-height .33)
 
-(defun aleksei/display-buffer-below-selected-then-fit-and-select (buffer &optional alist)
-  "Display BUFFER at the bottom of the window, fit the height to the
-content, and select the window."
-  (let ((window (display-buffer-below-selected buffer `((side . bottom)))))
-    (fit-window-to-buffer window (floor (frame-height) 2))
-    (select-window window)  ;; Select the window displaying the buffer
-    window))
+;; (defun aleksei/display-buffer-below-selected-then-fit-and-select (buffer &optional alist)
+;;   "Display BUFFER at the bottom of the window, fit the height to the
+;; content, and select the window."
+;;   (let ((window (display-buffer-below-selected buffer `((side . bottom)))))
+;;     (fit-window-to-buffer window (floor (frame-height) 2))
+;;     (select-window window)  ;; Select the window displaying the buffer
+;;     window))
 
-(defun aleksei/display-buffer-in-side-window (buffer &optional alist)
-  "Display BUFFER at the appropriate place depending on the current frame width"
-  (let* ((side-width (or (cdr (assq 'side-width alist)) aleksei/default-side-window-width))
-         (side-height (or (cdr (assq 'side-height alist)) aleksei/default-side-window-height))
-         (wide-frame-opts `(list
-                            (window-width . ,side-width)
-                            (side . left)))
-         (narrow-frame-opts `(list
-                              (window-height . ,side-height)
-                              (side . bottom))))
+;; (defun aleksei/display-buffer-in-side-window (buffer &optional alist)
+;;   "Display BUFFER at the appropriate place depending on the current frame width"
+;;   (let* ((side-width (or (cdr (assq 'side-width alist)) aleksei/default-side-window-width))
+;;          (side-height (or (cdr (assq 'side-height alist)) aleksei/default-side-window-height))
+;;          (wide-frame-opts `(list
+;;                             (window-width . ,side-width)
+;;                             (side . left)))
+;;          (narrow-frame-opts `(list
+;;                               (window-height . ,side-height)
+;;                               (side . bottom))))
 
-    (display-buffer-in-side-window
-     buffer
-     (append alist
-             (if (aleksei/2-columns-layout-p) wide-frame-opts narrow-frame-opts))
-     )))
+;;     (display-buffer-in-side-window
+;;      buffer
+;;      (append alist
+;;              (if (aleksei/2-columns-layout-p) wide-frame-opts narrow-frame-opts))
+;;      )))
 
-(defun aleksei/display-buffer-in-side-window-if-wide (buffer &optional alist)
-  (if (aleksei/2-columns-layout-p)
-      (display-buffer-in-side-window
-       buffer
-       (append alist  `(list (side . left)
-                        (window-width . ,aleksei/default-side-window-width))))
-    (display-buffer-same-window buffer alist)))
+;; (defun aleksei/display-buffer-in-side-window-if-wide (buffer &optional alist)
+;;   (if (aleksei/2-columns-layout-p)
+;;       (display-buffer-in-side-window
+;;        buffer
+;;        (append alist  `(list (side . left)
+;;                         (window-width . ,aleksei/default-side-window-width))))
+;;     (display-buffer-same-window buffer alist)))
 
-(defun aleksei/2-columns-layout-p ()
-  (>= (frame-width) 200))
+;; (defun aleksei/2-columns-layout-p ()
+;;   (>= (frame-width) 200))
 
-(defun aleksei/1-column-layout-p ()
-  (not (aleksei/2-columns-layout-p)))
+;; (defun aleksei/1-column-layout-p ()
+;;   (not (aleksei/2-columns-layout-p)))
 
-(setq display-buffer-alist
-      '(
-        ((or . ((derived-mode . process-menu-mode)
-                (derived-mode . flycheck-error-list-mode)
-                "\\*RE-Builder\\*"
-                ;; "\\*diff-hl"
-                ))
-         (display-buffer-reuse-mode-window
-          aleksei/display-buffer-below-selected-then-fit-and-select))
+;; (setq display-buffer-alist
+;;       '(
+;;         ((or . ((derived-mode . process-menu-mode)
+;;                 (derived-mode . flycheck-error-list-mode)
+;;                 "\\*RE-Builder\\*"
+;;                 ;; "\\*diff-hl"
+;;                 ))
+;;          (display-buffer-reuse-mode-window
+;;           aleksei/display-buffer-below-selected-then-fit-and-select))
 
-        ((or . ("\\*Org Agenda"
-                ))
-         (display-buffer-reuse-window
-          display-buffer-same-window))
+;;         ((or . ("\\*Org Agenda"
+;;                 ))
+;;          (display-buffer-reuse-window
+;;           display-buffer-same-window))
 
-        ((or . ("\\*ChatGPT"
-                "^magit-log"
-                "^magit-revision"
-                (derived-mode . magit-status-mode)
-                "\\*Man"))
-         (aleksei/display-buffer-in-side-window-if-wide))
+;;         ((or . ("\\*ChatGPT"
+;;                 "^magit-log"
+;;                 "^magit-revision"
+;;                 (derived-mode . magit-status-mode)
+;;                 "\\*Man"))
+;;          (aleksei/display-buffer-in-side-window-if-wide))
 
-        ((or . ("^\\*"
-                "Output\\*$"
-                (derived-mode . compilation-mode)
-                (derived-mode . comint-mode)))
-         (aleksei/display-buffer-in-side-window))
+;;         ((or . ("^\\*"
+;;                 "Output\\*$"
+;;                 (derived-mode . compilation-mode)
+;;                 (derived-mode . comint-mode)))
+;;          (aleksei/display-buffer-in-side-window))
 
-        (".*"
-         (display-buffer-reuse-mode-window))
+;;         (".*"
+;;          (display-buffer-reuse-mode-window))
 
-        ))
+;;         ))
