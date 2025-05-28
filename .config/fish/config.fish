292cc4bb876c8b53aac28e82a6919d167476e3b6
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-05-28 12:40:07 +0300

    Improve tmux configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -38,5 +38,22 @@ if status is-interactive
             printf "%b" (string join "\n" (vterm_old_fish_prompt))
             vterm_prompt_end
         end
+
+        #  not in Emacs, so setup tmux integration
+    else if begin
+            [ -n "$TMUX" ]; and string match -q -r "screen|tmux" "$TERM"
+        end
+        # Directory tracking and Prompt tracking
+        function tmux_prompt_end
+            printf "\033]133;A\033\\"
+        end
+        functions --copy fish_prompt tmux_old_fish_prompt
+        function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
+            # Remove the trailing newline from the original prompt. This is done
+            # using the string builtin from fish, but to make sure any escape codes
+            # are correctly interpreted, use %b for printf.
+            printf "%b" (string join "\n" (tmux_old_fish_prompt))
+            tmux_prompt_end
+        end
     end
 end
