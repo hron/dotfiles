2163548323ab8c7f2f7fd91781f09536627d7fef
Aleksei Gusev <aleksei.gusev@gmail.com>
2026-02-03 11:07:14 +0200

    Fix org-roam links

--- .config/emacs/lisp/init-org.el
+++ .config/emacs/lisp/init-org.el
@@ -163,7 +163,7 @@
          ("C-c n c" . org-roam-capture)
          ;; Dailies
          ("C-c n j" . org-roam-dailies-capture-today))
-  :config
+  :init
   ;; If you're using a vertical completion framework, you might want a more informative completion interface
   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   (org-roam-db-autosync-mode)
