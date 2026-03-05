5ef32b155e9f4808609ddc8c4404cd53a42a52d2
Aleksei Gusev <aleksei.gusev@gmail.com>
2026-03-05 13:30:32 +0200

    Use default-rgb theme for fish

--- .config/fish/conf.d/70-dark-mode.fish
+++ .config/fish/conf.d/70-dark-mode.fish
@@ -2,7 +2,7 @@ set -l WHITE_THEME_NAME
 set -l COLOR_SCHEME (qdbus org.freedesktop.portal.Desktop /org/freedesktop/portal/desktop org.freedesktop.portal.Settings.Read org.freedesktop.appearance color-scheme)
 
 if test $COLOR_SCHEME != 1
-    fish_config theme choose ayu --color-theme=light
+    fish_config theme choose default-rgb --color-theme=light
 else
-    fish_config theme choose auy --color-theme=dark
+    fish_config theme choose default-rgb --color-theme=dark
 end
