6c43d27cea883c855e2ec289a9a078d461152cd3
Aleksei Gusev <aleksei.gusev@gmail.com>
2026-04-25 17:25:36 +0300

    Update configs

--- .config/fish/conf.d/10-path.fish
+++ .config/fish/conf.d/10-path.fish
@@ -1,5 +1,5 @@
 if status is-login
-    for path in $HOME/bin $HOME/.local/bin $HOME/.cargo/bin
+    for path in $HOME/bin $HOME/.local/bin $HOME/.cargo/bin $HOME/.npm-global/bin
         if test -d $path
             fish_add_path $path
         end
