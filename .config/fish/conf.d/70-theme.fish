3c6572560b88fac2441dcfe0cab105fc19564b34
Aleksei Gusev <aleksei.gusev@gmail.com>
2026-04-26 10:50:06 +0300

    Add support for fish theme change on dark mode toggle

--- .config/fish/conf.d/70-theme.fish
+++ .config/fish/conf.d/70-theme.fish
@@ -1,1 +1,9 @@
 fish_config theme choose default-rgb
+
+function handle_theme_change --on-signal USR1
+    if test "$fish_terminal_color_theme" = dark
+        set -gx fish_terminal_color_theme light
+    else
+        set -gx fish_terminal_color_theme dark
+    end
+end
