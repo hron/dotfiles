91e8014d2a35d83c34ee8026b96b68b899dc0c35
Aleksei Gusev <aleksei.gusev@gmail.com>
2026-05-06 12:04:06 +0300

    Fix removing ~/.config

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -e
 
 
-rm -f $HOME/.bashrc $HOME/.config
+rm -rf $HOME/.bashrc $HOME/.config
 for each in $(find bin/ -mindepth 1 | egrep -v '#$|~$') $( ls -a1 | egrep -v '(^AstoNvim$|^install.sh$|^bin$|\.git$|\.gitignore$|\.gitmodules|^\.\.?$|\~$|#$)'); do
     [ -e $HOME/$each ] || ln -nfs `pwd`/$each $HOME/$each;
 done
